412ebf1da3460b3b10f4adf2472083fd2a18b480
inluminas <87386149+inluminas@users.noreply.github.com>
2025-05-28 23:01:55 +0600

    Update Hashing_2D.cpp

--- Hashing_2D.cpp
+++ Hashing_2D.cpp
@@ -14,33 +14,6 @@ void getprimebase(){
     }
 }
 
-
-
-class Hash{ // 1 indexed hashing
-public:
-    vector<ll>hash1,hash2;
-    int n;
-    void init(string s){ // for numbers -> vector<ll>s
-        n = s.size();
-        hash1.resize(n + 2, 0LL);
-        hash2.resize(n + 2, 0LL);
-        for(int i = 1; i <= n; i++){
-            hash1[i] = ((hash1[i - 1] * base1) % MOD1 + s[i - 1]) % MOD1;
-            hash2[i] = ((hash2[i - 1] * base2) % MOD2 + s[i - 1]) % MOD2;
-        }
-    }
-
-    ll queryhash(int l, int r){
-        ll h1 = 0LL, h2 = 0LL;
-
-        h1 = (hash1[r] - (hash1[l - 1] * pw1[r - l + 1]) % MOD1 + MOD1 ) % MOD1;
-        h2 = (hash2[r] - (hash2[l - 1] * pw2[r - l + 1]) % MOD2 + MOD2 ) % MOD2;
-
-        return (h1 << 32) | h2;
-        // return h1;
-    }
-};
-
 class Hash2D{ // 1 indexed hashing
 public:
     vector<vector<ll>>hash;
