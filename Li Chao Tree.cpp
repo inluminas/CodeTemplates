a2965e49442bacf737e1236d3ab0ec47b5d65aed
pranshuprodipta07@gmail.com <pranshuprodipta07@gmail.com>
2026-02-06 16:55:54 +0600

    updated the line segment update feature

--- Li Chao Tree.cpp
+++ Li Chao Tree.cpp
@@ -1,26 +1,55 @@
-ll h2[lmt];// if range is small than use h2[i]=i
-vector<pair<ll,ll>>tree(3*lmt,{0,1e14});
+ll h[lmt];// if range is small than use h[i]=i
 
-ll f(pair<ll,ll>line,ll x){//line.first=m,line.second=c,y=m*x+c
-  return line.first*x+line.second;
+struct Line{
+    int m, c; // y = mx + c, can also use ax + by + c = 0;
+    Line(){
+        m = 0, c = 0;
+    }
+    Line(int _m, int _c){
+        m = _m, c = _c;
+    }
+    int value(int x) return m * x + c;
+};
+
+const int lmt = 2e5 + 10;
+vector<Line>tree(lmt * 4, Line(0, 1e16));
+// for minimum in range Line(0, 1e16), for maximum in range Line(0, -1e16);
+
+void add_line(int at, int L, int R, Line nw){
+    int mid = (L + R) >> 1;
+    bool lft = nw.value(h[L]) < tree[at].value(h[L]);
+    bool md = nw.value(h[mid]) < tree[at].value(mid); 
+    // make a compare function if the values are too big to compare
+
+    if(!md) swap(nw, tree[at]);
+
+    if(L == R) return;
+    if(lft != md) add_line(at << 1, L, mid, nw);
+    else add_line(at << 1 | 1, mid + 1, R, nw);
 }
 
-void update(pair<ll,ll>line,ll L=1,ll R=n,ll at=1){
-  ll mid=(L+R)>>1;
-  bool lft=f(line,h2[L])<f(tree[at],h2[L]);
-  bool md=f(line,h2[mid])<f(tree[at],h2[mid]);
+// updating the line in the range h[l] to h[r]
+void update(int at, int L, int R, int l, int r, Line nw){
+    if(r < L or R < l or r < l) return;
+    if(l <= L and R <= r){
+        add_line(at, L, R, nw);
+        return;
+    }
 
-  if(md) swap(tree[at],line);
-
-  if(L==R) return;
-  else if(lft!=md) update(line,L,mid,at*2);
-  else update(line,mid+1,R,at*2+1);
+    int mid = (L + R) >> 1;
+    update(at << 1, L, mid, l, r, nw);
+    update(at << 1 | 1, mid + 1, R, l, r, nw);
 }
 
-ll query(ll x,ll L=1,ll R=n,ll at=1){
-  ll mid=(L+R)>>1;
-  ll cur=f(tree[at],x);
-  if(L==R) return cur;
-  if(x<=h2[mid]) return min(cur,query(x,L,mid,at*2));
-  else return min(cur,query(x,mid+1,R,at*2+1));
+Line query(int at, int L, int R, int x){
+    if(L == R) return tree[at];
+    
+    int mid = (L + R) >> 1;
+    Line res;
+    if(x <= h[mid]) res = query(at << 1, L, mid, x);
+    else res = query(at << 1 | 1, mid, R, x);
+
+    if(res.value(x) > tree[at].value(x)) swap(res, tree[at]);
+    // make a compare function if the values are too big to compare
+    return res;
 }
