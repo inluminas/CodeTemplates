ff114db919f954a67d308fe7e7604d84cf8c0429
inluminas <pranshuprodipta07@gmail.com>
2026-02-06 16:04:49 +0600

    updated as lazy propagation

--- HLD updated.cpp
+++ HLD updated.cpp
@@ -13,7 +13,7 @@ int dfs(int u, int p){
             mx=subtree;
             heavy[u]=v;
         }
-        sz+=subtree;
+        sz+=subtree;    
     }
     return sz;
 }
@@ -33,7 +33,18 @@ void hld(int u, int h){
     }
 }
 
+void propagate(int at, int L, int R){
+    if(!lazy[at]) return;
+    seg[at] += ((R - L + 1) * lazy[at]);
+    if(L != R){
+        lazy[at << 1] += lazy[at], lazy[at << 1 | 1] += lazy[at];
+    }
+    lazy[at] = 0;
+    return;
+}
+
 void build(int at,int L,int R){
+    lazy[at] = 0;
     if(L==R){
         seg[at]=b[L];
         return;
@@ -43,18 +54,22 @@ void build(int at,int L,int R){
     seg[at]=seg[at*2]+seg[at*2+1];
 }
 
-void update(int at,int L, int R, int pos, int val){
-    if(L==R){
-        seg[at]+=val;
+void update(int at, int L, int R, int l, int r, int val){
+    propagate(at, L, R);
+    if(r < l or R < l or r < L) return;
+    if(l <= L and R <= r){
+        lazy[at] += val;
+        propagate(at, L, R);
         return;
     }
-    int mid=(L+R)>>1;
-    if(pos<=mid) update(at*2,L,mid,pos,val);
-    else update(at*2+1,mid+1,R,pos,val);
-    seg[at]=seg[at*2]+seg[at*2+1];
+    int mid = (L + R) >> 1;
+    update(at << 1, L, mid, l, r, val);
+    update(at << 1 | 1, mid + 1, R, l, r, val);
+    seg[at] = seg[at << 1] + seg[at << 1 | 1];
 }
 
 int query(int at,int L,int R,int l,int r){
+    propagate(at, L, R);
     if(r<L or R<l or r<l) return 0LL;
     if(l<=L && R<=r) return seg[at];
     int mid=(L+R)>>1;
@@ -62,14 +77,25 @@ int query(int at,int L,int R,int l,int r){
     return (x+y);
 }
 
-int getans(int u, int v){
-    int res=0;
-    while(head[u]!=head[v]){
-        if(lvl[head[u]]<lvl[head[v]]) swap(u,v);
-        res+=query(1,1,n,pos[head[u]],pos[u]);
-        u=par[head[u]];
+void hldupdate(int u, int v, int val){
+    while(head[u] != head[v]){
+        if(dep[head[u]] < dep[head[v]]) swap(u, v);
+        update(1, 1, n, pos[head[u]], pos[u], val);
+        u = par[head[u]];
     }
-    if(lvl[u]>lvl[v]) swap(u,v);
-    res+=query(1,1,n,pos[u],pos[v]);
+    if(dep[u] > dep[v]) swap(u, v);
+    update(1, 1, n, pos[u], pos[v], val);
+    return;
+}
+
+int hldquery(int u, int v){
+    int res = 0;
+    while(head[u] != head[v]){
+        if(dep[head[u]] < dep[head[v]]) swap(u, v);
+        res += query(1, 1, n, pos[head[u]], pos[u]);
+        u = par[head[u]];
+    }
+    if(dep[u] > dep[v]) swap(u, v);
+    res += query(1, 1, n, pos[u], pos[v]);
     return res;
 }
