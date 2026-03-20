7b6676016565da0605acade08ca35f5be5c842f7
pranshuprodipta07@gmail.com <pranshuprodipta07@gmail.com>
2026-03-20 16:10:28 +0600

    Updated Sparse table

--- Sparse Table for RMQ.cpp
+++ Sparse Table for RMQ.cpp
@@ -10,7 +10,7 @@ void build(){
 
     for(int j = 1; j < 20; j++){
         for(int i = 1; i <= n; i++){
-            if(i + (1 << (j - 1)) - 1 <= n){
+            if(i + (1 << j) - 1 <= n){
                 mx[i][j] = max(mx[i][j - 1], mx[i + (1 << (j - 1))][j - 1]);
             }
         }
